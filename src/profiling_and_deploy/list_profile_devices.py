092e3dc10e82f7be3e42eca6fcd62968b8be8e6c
J. R. Verbiest <39417102+JrVerbiest@users.noreply.github.com>
2023-05-30 09:07:37 +0200

    Create list_profile_devices.py

---
+++ src/profiling_and_deploy/list_profile_devices.py
@@ -0,0 +1,18 @@
+import edgeimpulse as ei
+
+import os
+import yaml
+from yaml.loader import SafeLoader
+
+import logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+env_file = 'config.yaml'
+# from env (yaml) file
+with open(os.path.join(os.getcwd(), env_file), "r") as yaml_file:
+    env_config = yaml.load(yaml_file, Loader=SafeLoader)
+yaml_file.close()
+
+ei.API_KEY = env_config['ei']['key']
+logger.info(ei.model.list_profile_devices())
